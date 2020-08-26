aea7029529caee4bd97d0993dd5d06ecef304212
Hydr8O <bananov888@gmail.com>
2020-08-26 16:10:54 +0100

    make detections work without someone in browser

--- pi_stream/streaming_service.py
+++ pi_stream/streaming_service.py
@@ -10,7 +10,6 @@ class Streamer:
         self._current_frame = None
         self._alarm = Alarm(frames_with_no_detections_threshold)
         self._detector = detector
-     
     
     def stream(self):
         '''Starts to stream video from camera'''
@@ -26,9 +25,41 @@ class Streamer:
                 self._alarm.sound()
             else:
                 self._alarm.increment_frames_with_no_detections()
+    
             next_frame = self._construct_next_frame()
             yield next_frame
     
+
+    def _process_frame(self):
+        self._set_current_frame()
+        self._draw_detections_on_current_frame()
+            
+        if self._alarm.is_needed_to_refresh():
+            self._alarm.refresh()
+                
+        if self._detector.is_human() == True:
+            self._alarm.set_frames_with_no_detections_to_zero()
+            self._alarm.sound()
+        else:
+            self._alarm.increment_frames_with_no_detections()
+    
+    def start_frame_processing(self):
+        '''Root stream which always works'''
+        while True:
+            print('streaming')
+            self._set_current_frame()
+            self._draw_detections_on_current_frame()
+            
+            if self._alarm.is_needed_to_refresh():
+                self._alarm.refresh()
+                
+            if self._detector.is_object() == True:
+                self._alarm.set_frames_with_no_detections_to_zero()
+                self._alarm.sound()
+            else:
+                self._alarm.increment_frames_with_no_detections()
+    
+
     def _set_current_frame(self):
         '''Gets frame from camera and sets it as current frame'''
         self._current_frame = self._camera.get_frame()
--- pi_stream/views.py
+++ pi_stream/views.py
@@ -1,21 +1,25 @@
 import os
+import threading
 from django.shortcuts import render
 from django.http import StreamingHttpResponse
 from django.conf import settings
 import numpy as np
 from .opencv_detection_utils.FaceDetector import FaceDetector
+from .opencv_detection_utils.HumanDetector import HumanDetector
 from .streaming_service import Streamer
 from io import BytesIO
 import cv2
 from .camera_service.camera_pi import Camera
 
 face_path = settings.BASE_DIR / 'pi_stream' / 'face-config'
-proto_path = str(face_path / 'deploy.prototxt')
-model_path = str(face_path / 'res10_300x300_ssd_iter_140000.caffemodel')
-#detector = HumanDetector(scale=1.065, win_stride=(8, 8))
-detector = FaceDetector(proto_path, model_path, confidence=0.2)
-streamer = Streamer(Camera(), detector)
-
+proto_path1 = str(face_path / 'deploy.prototxt')
+proto_path2 = str(face_path / 'deploy.prototxt2')
+model_path1 = str(face_path / 'res10_300x300_ssd_iter_140000.caffemodel')
+model_path2 = str(face_path / 'res10_300x300_ssd_iter_140000.caffemodel2')
+detectors = {"detector1": False, "detector2": False}
+detector1 = FaceDetector(proto_path1, model_path1, confidence=.2)
+detector2 = FaceDetector(proto_path2, model_path2, confidence=.2)
+#detector = HumanDetector()
 def detect(image):
    detected = detector.detect_image(image)
    _, frame = cv2.imencode('.JPEG', detected.get_image())
@@ -27,9 +31,18 @@ def stream(request):
         'pi_stream/stream.html'
     ))
     
-def video_feed(request):   
-    streamer = Streamer(Camera(), detector)
+def video_feed(response):   
+    if detectors["detector1"] == False:
+        detectors["detector2"] = False
+        streamer = Streamer(Camera(), detector1)
+        detectors["detector1"] = True
+    else:
+        detectors["detector1"] = False
+        streamer = Streamer(Camera(), detector2)
+        detectors["detector2"] = True
     return(StreamingHttpResponse(
         streamer.stream(), 
         content_type='multipart/x-mixed-replace; boundary=frame'
     ))
+
+video_feed('-')
