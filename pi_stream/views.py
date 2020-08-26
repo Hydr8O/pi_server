26d461f72465be3947649789436982e83d4f3377
Hydr8O <bananov888@gmail.com>
2020-08-26 18:43:27 +0100

    remove detector manager

--- pi_stream/views.py
+++ pi_stream/views.py
@@ -5,15 +5,8 @@ from .opencv_detection_utils.FaceDetector import FaceDetector
 from .streaming_service import Streamer
 from .camera_service.camera_pi import Camera
 
-proto_path1 = str(settings.FACE_PATH / 'deploy.prototxt')
-proto_path2 = str(settings.FACE_PATH / 'deploy.prototxt2')
-model_path1 = str(settings.FACE_PATH / 'res10_300x300_ssd_iter_140000.caffemodel')
-model_path2 = str(settings.FACE_PATH / 'res10_300x300_ssd_iter_140000.caffemodel2')
-detectors = {"detector1": False, "detector2": False}
-detector1 = FaceDetector(proto_path1, model_path1, confidence=.2)
-detector2 = FaceDetector(proto_path2, model_path2, confidence=.2)
-streamers = []
-
+proto_path = str(settings.FACE_PATH / 'deploy.prototxt')
+model_path = str(settings.FACE_PATH / 'res10_300x300_ssd_iter_140000.caffemodel')
 
 def stream(request):
     return(render(
@@ -21,16 +14,9 @@ def stream(request):
         'pi_stream/stream.html'
     ))
     
-def video_feed(response):   
-    print(Camera().get_clients())
-    if detectors["detector1"] == False:
-        detectors["detector2"] = False
-        streamer = Streamer(Camera(), detector1)
-        detectors["detector1"] = True
-    else:
-        detectors["detector1"] = False
-        streamer = Streamer(Camera(), detector2)
-        detectors["detector2"] = True
+def video_feed(response):
+    detector = FaceDetector(proto_path, model_path, confidence=.2)
+    streamer = Streamer(Camera(), detector)
     return(StreamingHttpResponse(
         streamer.stream(), 
         content_type='multipart/x-mixed-replace; boundary=frame'
